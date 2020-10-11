c9378513fa044be719a992e53c6ca41e0836cd21
Walter <wdmira@hotmail.com>
2020-10-11 12:29:07 -0300

    docs agregado

---
+++ docs/precache-manifest.10b3e9b10581ad7d859881d14b03f249.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f844cf96c582bcdb925634417bf03e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9160f3fc392e33c7fbd",
+    "url": "/static/css/main.832776f2.chunk.css"
+  },
+  {
+    "revision": "2f019df55de657bdf252",
+    "url": "/static/js/2.acd19517.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.acd19517.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9160f3fc392e33c7fbd",
+    "url": "/static/js/main.370c5fd5.chunk.js"
+  },
+  {
+    "revision": "f752d0c94674938ff97b",
+    "url": "/static/js/runtime-main.9e05ee20.js"
+  }
+]);
